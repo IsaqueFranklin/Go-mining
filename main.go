75a48486d72996f9968cb3886928014f59dbab56
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2024-04-09 20:49:44 -0300

    POST method for blockchain data

--- main.go
+++ main.go
@@ -85,3 +85,28 @@ func respondWithJSON(w http.ResponseWriter, r *http.Request, code int, payload i
   w.WriteHeader(code)
   w.Write(response)
 }
+
+func handleWriteBlock(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Content-Type", "application/json")
+        var m Message
+
+        decoder := json.NewDecoder(r.Body)
+        if err := decoder.Decode(&m); err != nil {
+                respondWithJSON(w, r, http.StatusBadRequest, r.Body)
+                return
+        }   
+        defer r.Body.Close()
+
+        //ensure atomicity when creating new block
+        mutex.Lock()
+        newBlock := generateBlock(Blockchain[len(Blockchain)-1], m.BPM)
+        mutex.Unlock()
+
+        if isBlockValid(newBlock, Blockchain[len(Blockchain)-1]) {
+                Blockchain = append(Blockchain, newBlock)
+                spew.Dump(Blockchain)
+        }   
+
+        respondWithJSON(w, r, http.StatusCreated, newBlock)
+
+}
