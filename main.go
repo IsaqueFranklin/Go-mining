93fd220712a052e9635e2ebd1db3b00b14c7f159
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2024-04-09 20:56:23 -0300

    isHashValid func

--- main.go
+++ main.go
@@ -136,3 +136,8 @@ func calculateHash(block Block) string {
         hashed := h.Sum(nil)
         return hex.EncodeToString(hashed)
 }
+
+func isHashValid(hash string, difficulty int) bool {
+        prefix := strings.Repeat("0", difficulty)
+        return strings.HasPrefix(hash, prefix)
+}
